6431be672f2ab79409881c529fb9c929b42b0583
Tommy-Sand <rosansandanasamy@gmail.com>
2023-01-09 19:00:40 -0700

    Last commit

--- version_script.py
+++ version_script.py
@@ -1,5 +1,10 @@
 import requests
 
-print(requests.__version__)
+print("Requests library version: ", requests.__version__)
 
 google_homepage = requests.get("https://www.google.com")
+
+python_script = requests.get("https://raw.githubusercontent.com/Tommy-Sand/cmput404_lab1/main/version_script.py")
+
+print("Python script source code: ")
+print(python_script.text)
